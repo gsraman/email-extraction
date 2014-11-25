ddbb4a1aed566d1382ec654d04d335f813f18dc9
gsraman <gsriram13@gmail.com>
2014-11-25 16:16:19 +0530

    counting number of emails

--- email_extract.py
+++ email_extract.py
@@ -1,9 +1,11 @@
 print "Check if you are running this file from the same directory as the document"
 fname = raw_input('Enter the file name:')
 
+
 fh = open(fname)
 fn = open('result.txt','w')
 set1 = set()
+count = 0
 
 for line in fh:
 	line.rstrip()
@@ -11,9 +13,11 @@ for line in fh:
 		if '@' in word:
 			if '.' in word:
 				set1.add(word)
+				count += 1
 
 for item in set1:
 	fn.write(item)
 	fn.write("\n")
 
 print "Results stored in result.txt. Previous data will be overwritten (if any)"
+print count,"distinct email-ids extracted"
